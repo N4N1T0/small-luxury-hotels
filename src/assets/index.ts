e863a110771b80ceafedadfe967f4750d4f89883
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-04-26 11:41:22 +0200

    chore: add new images and update assets index file

---
+++ src/assets/index.ts
@@ -0,0 +1,24 @@
+// SVG
+import Logo from "./svg/logo.svg";
+import LogoWhite from "./svg/logo-white.svg";
+import HotelCiudad from "./svg/hotel-ciudad.svg";
+import HotelPlaya from "./svg/hotel-playa.svg";
+import HotelBosque from "./svg/hotel-bosque.svg";
+
+// IMAGES
+import HeroHome from "./images/spencer-everett-4MTSE39Oqro-unsplash (1).webp";
+import LaHabana1 from "./images/ariil-davydov-BlLOShuF2Lk-unsplash.webp";
+import LaHabana2 from "./images/max-letek-am8v7sW9HBk-unsplash.webp";
+import LaHabana3 from "./images/richard-hedrick-sE-5ApCJcQ4-unsplash.webp";
+
+export {
+  Logo,
+  HeroHome,
+  HotelCiudad,
+  HotelPlaya,
+  HotelBosque,
+  LaHabana1,
+  LaHabana2,
+  LaHabana3,
+  LogoWhite,
+};
