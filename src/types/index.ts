6c8388ead2cecb38b1bd7e7c50ad99fa4b4fd007
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-04-28 10:55:31 +0200

    feat(types): add Hotel and Feature interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Hotel {
+  id: string;
+  title: string;
+  description: string;
+  imageSrc: string;
+  imageAlt: string;
+  link: string;
+}
+
+export interface Feature {
+  icon: string;
+  title: string;
+  description: string;
+}
