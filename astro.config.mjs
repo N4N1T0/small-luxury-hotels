f9f05f8c0d6deb2abf6e6746c7cc58469d5da690
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-04-30 22:26:08 +0200

    build: update dependencies to latest versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
 
-import mdx from "@astrojs/mdx";
-
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [preact(), mdx()],
+  integrations: [preact()],
 });
