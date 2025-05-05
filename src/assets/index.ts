568d2a1d376ba18b1ee4b3393842694a8c5c4d68
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-05-05 10:59:58 +0200

    chore: remove unused image imports and update assets index

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,20 +5,4 @@ import HotelCiudad from "./svg/hotel-ciudad.svg";
 import HotelPlaya from "./svg/hotel-playa.svg";
 import HotelBosque from "./svg/hotel-bosque.svg";
 
-// IMAGES
-import HeroHome from "./images/spencer-everett-4MTSE39Oqro-unsplash (1).webp";
-import LaHabana1 from "./images/ariil-davydov-BlLOShuF2Lk-unsplash.webp";
-import LaHabana2 from "./images/max-letek-am8v7sW9HBk-unsplash.webp";
-import LaHabana3 from "./images/richard-hedrick-sE-5ApCJcQ4-unsplash.webp";
-
-export {
-  Logo,
-  HeroHome,
-  HotelCiudad,
-  HotelPlaya,
-  HotelBosque,
-  LaHabana1,
-  LaHabana2,
-  LaHabana3,
-  LogoWhite,
-};
+export { Logo, HotelCiudad, HotelPlaya, HotelBosque, LogoWhite };
