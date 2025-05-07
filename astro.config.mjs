46c79ae4cbbeb1eb952bfb1d336dcbdbabe111cf
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-05-08 01:05:14 +0200

    build: update dependencies and add sitemap, robots.txt, and compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,21 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
 
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
+
+import playformCompress from "@playform/compress";
+
 export default defineConfig({
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [preact()],
+  integrations: [
+    preact(),
+    sitemap(),
+    robotsTxt(),
+    playformCompress(),
+  ],
 });
