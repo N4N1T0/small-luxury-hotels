87f042097cb30d107718aea9f84791699abe349d
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-05-08 00:25:58 +0200

    feat(home): add hero image carousel with hotel images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -9,6 +9,15 @@ import HotelBosque from "./svg/hotel-bosque.svg";
 import HeroPlaceholder from "./placeholders/Hero.png";
 import Square from "./placeholders/Square.png";
 
+// HOTELS
+import Hotel1 from "./hoteles/boutique-bohemia/Bohemia_FAC7619.webp";
+import Hotel2 from "./hoteles/jesus-maria-7/DISTRIBIDOR Y FACHADA PAULA DE NOCHE.webp";
+import Hotel3 from "./hoteles/paseo-206/_ZNT2901.webp";
+import Hotel4 from "./hoteles/boutique-bohemia/Bohemia_FAC7575.webp";
+import Hotel5 from "./hoteles/jesus-maria-7/PATIO DE NOCHE 2.webp";
+import Hotel6 from "./hoteles/paseo-206/_ZNT3748.webp";
+const heroHotels = [Hotel1, Hotel2, Hotel3, Hotel4, Hotel5, Hotel6];
+
 export {
   Logo,
   HotelCiudad,
@@ -17,4 +26,5 @@ export {
   LogoWhite,
   HeroPlaceholder,
   Square,
+  heroHotels,
 };
